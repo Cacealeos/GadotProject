3b371d7d7d829e65e646a5d8da6431a2847e806e
Portakin <54554311+Cacealeos@users.noreply.github.com>
2024-06-29 21:57:37 -0500

    added improvised collision logic

--- Scripts/targetCollision.gd
+++ Scripts/targetCollision.gd
@@ -1,28 +1,33 @@
 extends Node2D
 
 var element = preload("res://Element_loader.tscn")
+
 var elementalArray = ["crate","cargo","shark","crab"]
 var queuetracker = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	collision()
 	pass # Replace with function body.
 
-func _collision():
-	element.instantiate()
-	element.position = (Vector2(0,0))
-	element.Element.texture = _applyTexture(elementalArray[queuetracker])
+func collision():
+	var instance = element.instantiate()
+	instance.position = (Vector2(77,33))
+	instance.texture = applyTexture(elementalArray[queuetracker])
+	add_child(instance)
+	#main.Roster.RosterElement1.hide()
 	++queuetracker
 
-func _applyTexture(index):
+func applyTexture(index):
 	match index:
 		"crate":
-			return "res://crate-removebg-preview.png"
+			return load("res://Assets/crate-removebg-preview.png")
 		"cargo":
-			return "res://cargo-removebg-preview.png"
+			return  load("res://Assets/cargo-removebg-preview.png")
 		"shark":
-			return "res://shark-removebg-preview.png"
+			return  load("res://Assets/sharkbox-removebg-preview.png")
 		"crab":
-			return "res://crab-removebg-preview.png"
+			return  load("res://Assets/crabtrap-removebg-preview.png")
 		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
